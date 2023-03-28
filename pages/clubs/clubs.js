b00eebea3be17ac37484637d3aae0820870e730d
FHallengreen <fred70p9@stud.kea.dk>
2023-03-28 10:09:51 +0200

    Smaller changes to login and club

--- pages/clubs/clubs.js
+++ pages/clubs/clubs.js
@@ -52,7 +52,7 @@ export async function initClubs() {
     // attach onclick event to each club-box element
     const clubBoxes = document.getElementsByClassName("club-box");
     for (let i = 0; i < clubBoxes.length; i++) {
-      clubBoxes[i].addEventListener("click", function() {
+      clubBoxes[i].addEventListener("dblclick", function() {
         showInfo(this);
       });
     }

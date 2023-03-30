83bb9a9b8b0bf74bd2ca653bb9d4350f39b4c6e8
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-03-30 13:26:29 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://api.funskate.dk"
-export const API_URL = "http://localhost:8080"
+export const API_URL = "https://api.funskate.dk"
+//export const API_URL = "http://localhost:8080"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
 
